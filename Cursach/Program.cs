5abcd48b1715864200db5b22dcf2223dff19a8e7
ArtemRybak <artemjet@gmail.com>
2024-04-22 18:42:58 +0300

    init

--- Cursach/Program.cs
+++ Cursach/Program.cs
@@ -12,6 +12,7 @@ namespace Cursach
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            //Ratata
         }
     }
 }
