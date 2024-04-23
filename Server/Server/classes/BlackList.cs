be8071068b071732273f81032eb01bf1404e92fa
MykhailoGrygorenko <osnova2021@gmail.com>
2024-04-23 19:23:31 +0300

    added classes

---
+++ Server/Server/classes/BlackList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.classes
+{
+    public class BlackList
+    {
+        public int BlockerId { get; set; }
+        public User Blocker { get; set; }
+        public int BlockedId { get; set; }
+        public User Blocked { get; set; }
+    }
+}
